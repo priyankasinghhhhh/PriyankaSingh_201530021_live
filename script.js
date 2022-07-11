d3231b56981244cf673db7c629c1aaa9a7cc7c6d
priyankasinghhhhh <109071543+priyankasinghhhhh@users.noreply.github.com>
2022-07-11 21:46:54 +0530

    All files are uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+var Name = "Priyanka Singh";
+var roll = "201530021";
+var date = new Date().toLocaleDateString();
+var clg = "GLA University, Mathura";
+document.getElementById('name').innerHTML = Name;
+document.getElementById('roll').innerHTML = roll;
+document.getElementById('clg').innerHTML = clg;
+document.getElementById('date').innerHTML = date;
